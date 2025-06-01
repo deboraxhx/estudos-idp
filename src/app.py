442ed0e84b80449ef27baa4cd2206c30e83ac1a8
deboraxhx <debora.laiber@gmail.com>
2025-06-01 19:21:47 -0300

    alteracoes no arquivo ci

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'You are doing great, Debora! <3',
+        'message': 'You are doing great, Debora! so vamos <3',
         'deployed_on': 'kubernetes'
     })
 
